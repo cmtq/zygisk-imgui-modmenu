5b4a614c196fe5b7294a4fb4cf127f845b1ed2d0
cmtq <cmtq3@proton.me>
2025-02-10 21:33:28 +0300

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -47,6 +47,16 @@ void SetupImGui() {
     ImGui::GetStyle().ScaleAllSizes(3.0f);
 }
 
+void RenderGUI()
+{
+    ImGui::Begin("Button Menu");
+
+    MyButtonMenu(); // Call your button menu function
+
+    ImGui::End();
+}
+
+
 EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
 EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
     eglQuerySurface(dpy, surface, EGL_WIDTH, &g_GlWidth);
@@ -94,14 +104,6 @@ void MyButtonMenu()
 // ...
 
 // Call the function that contains your button menu
-void RenderGUI()
-{
-    ImGui::Begin("Button Menu");
-
-    MyButtonMenu(); // Call your button menu function
-
-    ImGui::End();
-}
 
 
 void hack_start(const char *_game_data_dir) {
